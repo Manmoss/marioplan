ff602e5ac8a24ff86fd69e95aa67a7dcee698019
志田守 <mamoru.bit0926@gmail.com>
2018-10-21 20:11:15 +0900

    Setup Formatting Dates

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,16 +2,18 @@ export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore.collection('projects').add({
       ...project, 
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT', project });
     }).catch((err) => {
-      dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err });
     })
   }
 };
