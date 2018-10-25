f29d1a303841c7fe10f20e5bec0e333a107dc9ae
志田守 <mamoru.bit0926@gmail.com>
2018-10-25 22:33:12 +0900

    Auth trigger

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const createNotification = (notification => {
   return admin.firestore().collection('notifications')
   .add(notification)
   .then(doc => console.log('notificaiton added', doc));
-})
+});
 
 exports.projectCreated = functions.firestore
   .document('projects/{projectId}')
@@ -24,5 +24,24 @@ exports.projectCreated = functions.firestore
     }
 
     return createNotification(notification);
+
+})
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification);
+
+      })
 
 })
